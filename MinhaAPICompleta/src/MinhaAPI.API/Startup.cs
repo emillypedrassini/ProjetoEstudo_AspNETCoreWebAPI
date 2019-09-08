37f10ec3083478edaccd336ac99dcfc6ea85a7a3
emilly carolini pedrassini <emillypedrassini@gmail.com>
2019-09-07 23:32:10 -0300

    Configurando Injeção de Dependência

--- MinhaAPICompleta/src/MinhaAPI.API/Startup.cs
+++ MinhaAPICompleta/src/MinhaAPI.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MinhaAPI.API.Configuration;
 
 namespace MinhaAPI.API
 {
@@ -30,6 +31,8 @@ namespace MinhaAPI.API
             services.AddAutoMapper(typeof(Startup));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
