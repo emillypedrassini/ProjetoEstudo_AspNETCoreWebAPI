520167d86e38d0e6666a2bb3bfbef359f5e5e523
emilly carolini pedrassini <emillypedrassini@gmail.com>
2019-09-07 23:16:02 -0300

    Add Diretórios

---
+++ MinhaAPICompleta/src/MinhaAPI.API/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaAPI.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validações de notificações de erro
+
+        //validações de modelstate
+
+        //validações da operação de negocio
+    }
+}
