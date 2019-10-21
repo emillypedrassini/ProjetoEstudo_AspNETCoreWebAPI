b253a2340cf6df6a98bafa41047178a61eeafbab
emilly carolini pedrassini <emillypedrassini@gmail.com>
2019-10-21 19:56:29 -0300

    Adicionando as Entidades do Dominio

---
+++ MinhaAPICompleta/src/MinhaAPI.Business/Models/Fornecedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinhaAPI.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public bool Ativo { get; set; }
+
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        /* EF Relations */
+        public Endereco Endereco { get; set; }
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+
+    }
+}
